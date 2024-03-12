98973f430699f583cfc5166bf157c749ab7c0560
Yazmin <yazminmoniquetorres@gmail.com>
2024-03-12 11:09:02 -0700

    add example file for tests

---
+++ app/calculations.py
@@ -0,0 +1,31 @@
+def add(num1: int, num2: int):
+    return num1 + num2
+
+def subtract(num1: int, num2: int):
+    return num1 - num2
+
+def multiply(num1: int, num2: int):
+    return num1 * num2
+
+def divide(num1: int, num2: int):
+    return num1 / num2
+
+class InsufficientFunds(Exception):
+    pass
+
+class BankAccount():
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance < amount:
+            raise InsufficientFunds("Insufficient funds in account.")
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *= 1.1
+    
+
