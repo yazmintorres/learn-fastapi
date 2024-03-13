f415f743a3f2bd36b9860e7b4cfec1323dfa5a5e
Yazmin <yazminmoniquetorres@gmail.com>
2024-03-12 22:54:16 -0700

    change to test CI/CD pipeline

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Finally figured out deploying to Ubuntu with CI/CD!"}
+    return {"message": "Deployed via CI/CD pipeline!"}
 
 # @app.get("/sqlalchemy")
 # def test_posts(db: Session = Depends(get_db)):
